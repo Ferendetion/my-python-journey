5d583c01be8ff59277d77b3546fd60571e1a946e
Hachio <andhikadwis171@gmail.com>
2025-06-01 15:34:17 +0700

    Create simple-calculator.py
    Learning Function & Return Values

---
+++ simple-calculator.py
@@ -0,0 +1,25 @@
+def kalkulator():
+    try:
+        a = int(input("Masukkan angka :"))
+        operasi = input("Pilih (+, -, *, /): ")
+        b = int(input("Masukkan angka :"))
+
+        if operasi == "+":
+            return a + b
+        elif operasi == "-":
+            return a - b
+        elif operasi == "*":
+            return a * b
+        elif operasi == "/":
+            return a / b
+        else:
+            return "Error: operasi tidak valid"
+
+    except ValueError: #output ketika pengguna memasukkan huruf
+        return "Beri input dengan benar"
+    except ZeroDivisionError: #output ketika pengguna membagi dengan angka 0
+        return "Jangan membagi dengan 0"
+
+
+hasil = kalkulator()
+print(hasil)
